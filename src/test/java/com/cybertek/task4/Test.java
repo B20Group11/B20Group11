1f7c244068e72d9122099893800749cb9500f279
Alex-B-I <alex.berloh@gmail.com>
2020-09-21 14:31:21 -0500

    HI

--- src/test/java/com/cybertek/task4/Test.java
+++ src/test/java/com/cybertek/task4/Test.java
@@ -3,5 +3,9 @@ package com.cybertek.task4;
 public class Test {
     public static void main(String[] args) {
         System.out.println("Hello");
+
+
+        System.out.println("Hi Sergi");
     }
+
 }
