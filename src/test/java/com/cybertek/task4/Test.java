de327356be3741a50972393a6454585e76301ca3
“RadojeLekic” <raskelekic89@gmail.com>
2020-09-21 16:22:16 -0500

    adds

--- src/test/java/com/cybertek/task4/Test.java
+++ src/test/java/com/cybertek/task4/Test.java
@@ -4,8 +4,9 @@ public class Test {
     public static void main(String[] args) {
         System.out.println("Hello");
 
-
+        System.out.println("jeaaaa");
         System.out.println("Hi Sergi");
     }
+
 
 }
