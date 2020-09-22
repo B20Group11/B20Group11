2013ed937e1f7b9754c1b9d38c67dac3ec60914d
Alex-B-I <alex.berloh@gmail.com>
2020-09-21 20:12:09 -0500

    Everything Work

--- src/test/java/com/cybertek/task4/SmokeTest.java
+++ src/test/java/com/cybertek/task4/SmokeTest.java
@@ -1,12 +1,10 @@
 package com.cybertek.task4;
 
 import com.cybertek.utilities.WebDriverFactory;
-import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.interactions.Actions;
 import org.testng.Assert;
 import org.testng.annotations.AfterMethod;
