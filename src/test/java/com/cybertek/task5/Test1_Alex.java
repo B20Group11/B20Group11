4dbb44f1b5dfb3bbab1c575369bf0721b5c14eef
Alex-B-I <alex.berloh@gmail.com>
2020-10-01 14:36:09 -0500

    Alex test1

--- src/test/java/com/cybertek/task5/Test1_Alex.java
+++ src/test/java/com/cybertek/task5/Test1_Alex.java
@@ -3,10 +3,12 @@ package com.cybertek.task5;
 import com.cybertek.Credentials.Bitrix;
 import com.cybertek.utilities.BrowserUtils;
 import com.cybertek.utilities.WebDriverFactory;
+import com.github.javafaker.Faker;
 import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.testng.Assert;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
@@ -32,14 +34,31 @@ public class Test1_Alex {
         BrowserUtils.wait(1);
         driver.findElement(By.id("feed-add-post-form-tab-tasks")).click();
 
-        WebElement passwordInput = driver.findElement(By.xpath("//*[@data-bx-id='task-edit-title']"));
-        passwordInput.sendKeys("Hello group 11"+ Keys.ENTER);
+        Faker faker = new Faker();
+        String thingsToDo = faker.book().title();
 
-        WebElement checkbox = driver.findElement(By.xpath("//div[@class='task-info-panel-important']"));
-        checkbox.click();
+        BrowserUtils.wait(1);
+        WebElement taskTitle = driver.findElement(By.xpath("//*[@data-bx-id='task-edit-title']"));
+        taskTitle.sendKeys(thingsToDo+ Keys.ENTER);
 
+        BrowserUtils.wait(1);
+        WebElement checkbox1 = driver.findElement(By.xpath("//div[@class='task-info-panel-important']"));
+        checkbox1.click();
+
+        BrowserUtils.wait(1);
         WebElement sendButton = driver.findElement(By.id("blog-submit-button-save"));
         sendButton.click();
+
+        BrowserUtils.wait(1);
+        WebElement taskNameLink= driver.findElement(By.xpath("//a[.='"+thingsToDo+"']"));
+        taskNameLink.click();
+
+        WebElement checkbox2 = driver.findElement(By.xpath("//*[@id=\"task-detail-important-button\"]"));
+
+        System.out.println(checkbox2.getText());
+        ////*[@id="task-detail-important-button"]/span[2]
+
+        //Assert.assertTrue(checkbox2.isSelected(),"Checkbox2 is NOT selected. Verification FAILED!");
 
     }
 
