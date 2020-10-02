0e09a4209956279afc7ebd77dffbc529f2b79523
MeerimMoidin <ramissara18@gmail.com>
2020-10-02 00:34:31 -0500

    MeerimBitrix.java

---
+++ src/test/java/com/cybertek/task4/MeerimBitrix.java
@@ -0,0 +1,43 @@
+package com.cybertek.task4;
+
+import com.cybertek.Credentials.Bitrix;
+import com.cybertek.Credentials.VyTrack;
+import com.cybertek.utilities.WebDriverFactory;
+import org.openqa.selenium.Alert;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import java.nio.file.WatchEvent;
+import java.util.concurrent.TimeUnit;
+
+public class MeerimBitrix {
+
+    WebDriver driver;
+    @BeforeMethod
+    public void setUp(){
+        driver = WebDriverFactory.getDriver("chrome");
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        driver.get(" https://login2.nextbasecrm.com/stream/");
+        Bitrix.helpdesk(driver);
+    }@Test
+    public void us1Ac1(){
+        WebElement message=driver.findElement(By.xpath("//span[.='Message']"));
+        message.click();
+        WebElement uploadFiles=driver.findElement(By.xpath("//span[@id='bx-b-uploadfile-blogPostForm']"));
+        uploadFiles.click();
+        WebElement upload=driver.findElement(By.xpath("//td[@class='diskuf-selector wd-fa-add-file-light-cell wd-fa-add-file-from-main']"));
+        upload.click();
+    }
+    @AfterMethod
+    public void tearDownMethod() throws InterruptedException {
+
+        Thread.sleep(1000);
+        driver.close();
+}
+
+}
