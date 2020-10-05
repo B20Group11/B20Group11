d57e25ca6dd79b32447fe382c2cad56090b20b70
Alex-B-I <alex.berloh@gmail.com>
2020-10-05 15:27:00 -0500

    Update Test Alex

--- src/test/java/com/cybertek/task5/Test1_Alex.java
+++ src/test/java/com/cybertek/task5/Test1_Alex.java
@@ -46,12 +46,15 @@ public class Test1_Alex {
         WebElement checkbox1 = driver.findElement(By.xpath("//div[@class='task-info-panel-important']"));
         checkbox1.click();
 
+
+
         BrowserUtils.wait(1);
         WebElement sendButton = driver.findElement(By.id("blog-submit-button-save"));
         sendButton.click();
 
         WebElement popButton = driver.findElement(By.xpath("//*[@id=\"blogPostEditCreateTaskPopup\"]/div[2]/span"));
         popButton.click();
+        BrowserUtils.wait(2);
 /*
 
         BrowserUtils.wait(1);
@@ -64,10 +67,32 @@ public class Test1_Alex {
 
 
          */
-        WebElement checkbox2 = driver.findElement(By.xpath("//*[@id=\"task-detail-important-button\"]"));
+        //driver.switchTo().frame(0);
+       // driver.switchTo().frame(driver.findElement(By.id("iframe_jmrrrgkmc5")));
+      // WebElement checkHighPriority = driver.findElement(By.xpath("//div[@class='task-info-panel-important mutable']"));
+       // WebElement checkHighPriority = driver.findElement(By.xpath("//span[contains(@class, 'if-not-no')]"));
+        WebElement iframe = driver.findElement(By.className("side-panel-iframe"));
+        driver.switchTo().frame(iframe);
 
-        System.out.println(checkbox2.getText());
+       // WebElement checkHighPriority = driver.findElement(By.xpath("//span[contains(@class, 'if-not-no')]"));
+
+        //Assert.assertTrue(checkHighPriority.isSelected(),"Checkbox2 is NOT selected. Verification FAILED!");
+       // checkHighPriority.isSelected();
+
+        //WebElement checkHighPriority = driver.findElement(By.id("task-detail-important-button"));
+        //checkHighPriority.click();
+
+        WebElement checkHighPriority = driver.findElement(By.xpath("//div[@class='task-info-panel-important mutable']"));
+       // WebElement element = driver.findElement(By.id("task-detail-important-button"));
+       // String text = element.getText();
+       // System.out.println(text);
+
+
+       // WebElement element = driver.findElement(By.className("task-view-button timer-start ui-btn ui-btn-success"));
+       // element.click();
+       // System.out.println(checkHighPriority.getText());
         ////*[@id="task-detail-important-button"]/span[2]
+        //"//div[@class='task-info-panel-important mutable']"
 
         //Assert.assertTrue(checkbox2.isSelected(),"Checkbox2 is NOT selected. Verification FAILED!");
 
